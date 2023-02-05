1579418aa0ee37b282a916a4f73caa4f4f2d7a46
Teodoro Matiuzzi <89231860+BitsYoutubers@users.noreply.github.com>
2023-02-04 21:36:03 -0300

    Update convertorEspacial.js

--- convertorEspacial.js
+++ convertorEspacial.js
@@ -1,5 +1,8 @@
-var tempoAnoLuzTerra = 33;
-var metrosEmAnoLuz = 94605284000000000000000;
-
-var TerraAPlutao = metrosEmAnoLuz * tempoAnoLuzTerra;
-alert("A distância em metros da Terra até Plutão é de: " + TerraAPlutao);
+function converterLuz() {
+  var cotacaoAnosLuz = 9.461e15;
+  var nome = document.getElementById("nome").value;
+  var anosLuz = document.getElementById("valor").value;
+  var resultado = anosLuz * cotacaoAnosLuz;
+  var load = document.getElementById("load");
+  load.innerHTML = `<h3>Olá ${nome}!!<br> O resultado de Anos-Luz para Quilômetros: ${resultado}</h3>`;
+}
